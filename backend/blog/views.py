aa957be48c67910943b4a387d4b7cce1b7757cd9
Edward tladi <eddymalegale@gmail.com>
2022-03-02 16:32:45 +0200

    changes

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,5 @@
+from signal import raise_signal
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from .forms import *
@@ -8,8 +10,18 @@ class Post(View):
     def get(self, request, *args, **kwargs):
 
         form = postForm()
-
+         
         return render(request, "blog/blog.html", {'FORM': form})
 
     def post(self, request, *args, **kwargs):
-        pass
+        
+        try:
+
+            form = postForm(request.POST)
+
+        except:
+            return 
+
+        if form.is_valid(raise_signal):
+            form.save()
+            return HttpResponse("\success\\")
